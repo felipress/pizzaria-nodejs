c113f697442f01c803bed9fc83f7ebd11e53b7a9
Felipe <phellyype@gmail.com>
2024-09-14 09:49:26 -0300

    Feat: Config of index and packages install

---
+++ index.js
@@ -0,0 +1,18 @@
+// DEPENDENCIES
+const express = require("express")
+
+// DOTENV
+const dotenv = require("dotenv")
+dotenv.config()
+
+// EXPRESS START
+const app = express()
+const port = 3000
+
+// USAGE OF JSON
+app.use(express.json())
+
+// SERVER START
+app.listen(port, () => {
+    console.log(`O servidor está rodadndo em http://localhost:${port}`)
+})
