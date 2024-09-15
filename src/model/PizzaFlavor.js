24778bd63e6c1743517c38c1afa9f14903b31018
Felipe <phellyype@gmail.com>
2024-09-15 09:51:40 -0300

    Fix: include description to pizza flavor model

--- src/model/PizzaFlavor.js
+++ src/model/PizzaFlavor.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose")
 
 const PizzaFlavorSchema = new mongoose.Schema({
     flavor: {type: String, unique: true, required: true},
+    description: {type: String, required: true},
     price: {type: Number, required: true},
     available: {type: Boolean, required: true, default: true},
     createdAt: {type: Date, required: true, default: Date.now()}
